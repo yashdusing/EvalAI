db8115f2dd1d2be14b9c3f96670697c43bb157b2
Yash Dusing <yashdusing145@gmail.com>
2020-06-30 20:56:27 +0530

    Testing

--- gulpfile.js
+++ gulpfile.js
@@ -226,7 +226,7 @@ config for vm machines
 */
 function configVm() {
     configJson.vm.EnvironmentConfig.API = process.env.DJANGO_SERVER_URL;
-    console.log("Hey Yash ! Django server at ", configJson.vm.EnvironmentConfig.API);
+    console.log("Hey Yash ! Django server at ", process.env.DJANGO_SERVER_URL);
     return gulp.src('frontend/src/js/config.sample.js')
         .pipe(replace('moduleName', 'evalai-config'))
         .pipe(replace('constantName', Object.keys(configJson.vm)))
