28b4b28b05dd2f87132cd5224fa9953198dea662
Yash Dusing <yashdusing145@gmail.com>
2020-08-15 18:49:31 +0530

    temp

--- docker/vm/django/container-start.sh
+++ docker/vm/django/container-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput  && \
 python manage.py collectstatic --noinput  && \
-uwsgi --ini /code/docker/vm/django/uwsgi.ini
+python manage.py runserver 0.0.0.0:8000
