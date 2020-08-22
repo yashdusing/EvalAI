99b2d4f8d0d598371baa1f041d1eb9a33d4d3fd6
Yash Dusing <yashdusing145@gmail.com>
2020-08-22 15:48:57 +0530

    Shift to dev

--- docker/vm/django/container-start.sh
+++ docker/vm/django/container-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python manage.py migrate --noinput  && \
+cd /code && \
+python manage.py migrate --noinput && \
 python manage.py seed && \
-python manage.py collectstatic --noinput  && \
 python manage.py runserver 0.0.0.0:8000
