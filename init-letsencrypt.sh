778d2d5e1ea049064f430d705890a548747cc60a
Yash Dusing <yashdusing145@gmail.com>
2020-08-24 19:19:40 +0530

    Take input from screen

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -16,7 +16,10 @@ read -p "Skip reading variable (not for first time usage)? (y/N) " skip_input
     read -p "Enter AWS secret access key : " AWS_SECRET_ACCESS_KEY
     echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
 
-    read -p "Enter Hostname : " HOSTNAME
+    read -p "Enter Django Server (backend) : " DJANGO_SERVER
+    echo "DJANGO_SERVER=$DJANGO_SERVER" >> .env
+
+    read -p "Enter Hostname (frontend) : " HOSTNAME
     echo "HOSTNAME=$HOSTNAME" >> .env
 
     read -p "Enter RDS Hostname : " RDS_HOSTNAME
@@ -42,6 +45,33 @@ read -p "Skip reading variable (not for first time usage)? (y/N) " skip_input
 
     read -p "Enter Sentry URL : " SENTRY_URL
     echo "SENTRY_URL=$SENTRY_URL" >> .env
+
+    read -p "Enter ECS Cluster Name : " CLUSTER
+    echo "CLUSTER=$CLUSTER" >> .env
+
+    read -p "Enter ECS Execution Role ARN (eg : arn:aws:iam::{account_id}:role/ecsTaskExecutionRole) : " EXECUTION_ROLE_ARN
+    echo "EXECUTION_ROLE_ARN=$EXECUTION_ROLE_ARN" >> .env
+
+    read -p "Enter Subnet 1 ID : " SUBNET_1
+    echo "SUBNET_1=$SUBNET_1" >> .env
+
+    read -p "Enter Subnet 2 ID : " SUBNET_2
+    echo "SUBNET_2=$SUBNET_2" >> .env
+
+    read -p "Enter Subnet Security Group ID : " SUBNET_SECURITY_GROUP
+    echo "SUBNET_SECURITY_GROUP=$SUBNET_SECURITY_GROUP" >> .env
+
+    read -p "Enter Worker Image (yashdusing/evalai-aws-worker:latest) : " WORKER_IMAGE
+    echo "WORKER_IMAGE=$WORKER_IMAGE" >> .env
+
+    read -p "Enter Email Host (eg : email-smtp.us-east-1.amazonaws.com) : " EMAIL_HOST
+    echo "EMAIL_HOST=$EMAIL_HOST" >> .env
+
+    read -p "Enter Email Host Name : " EMAIL_HOST_USER
+    echo "EMAIL_HOST_USER=$EMAIL_HOST_USER" >> .env
+
+    read -p "Enter Email Host Password : " EMAIL_HOST_PASSWORD
+    echo "EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD" >> .env
 
     read -p "Enter your domain name (example.com) : " DOMAIN_NAME
     echo "DOMAIN_NAME=$DOMAIN_NAME" >> .env
